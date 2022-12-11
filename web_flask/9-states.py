0f28273475ede9a00b3e26a24d2427ed5cc8fa40
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-10 21:29:35 -0500

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+import os
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Removes the current SQLAlquemy session """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states_route():
+    """ Displays a HTML page """
+    states = [state for state in storage.all(State).values()]
+    return render_template(
+            '9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_id(id):
+    """ displays a HTML page """
+    states = [state for state in storage.all(State).values()]
+    list_ids = [state.id for state in storage.all(State).values()]
+    cities = [city for city in list(storage.all(City).values())]
+    return render_template(
+        '9-states.html',
+        cities=cities,
+        states=states,
+        id=id, list_ids=list_ids)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
